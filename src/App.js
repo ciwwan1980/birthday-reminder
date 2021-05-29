f8ca23f46def2d790a315ed455819face689084f
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-29 23:31:01 +0200

    importing-list-creating-button-for-clear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import data from './data';
+import List from "./List"
 
 function App() {
-  return <div>
-    {console.log(data)}
-  
-  <h3>reminder project setup</h3>
-  </div>;
+  return <main>
+  <section className="container"> 
+  <h3>0 birthdays today</h3>
+  <List />
+  <button
+  onClick={()=>console.log("you clikced me")}
+  >clear all</button>
+  </section>
+  </main>;
 }
 
 export default App;
