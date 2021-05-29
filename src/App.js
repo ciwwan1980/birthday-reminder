43431483548b4e91dcf4d750655b40688eb00aab
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-29 23:36:26 +0200

    passing-date-to-people-with-hooks-&&-display.people-array-length

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import data from './data';
 import List from "./List"
 
 function App() {
+
+  const [people,setPeople]=useState(data)
   return <main>
   <section className="container"> 
-  <h3>0 birthdays today</h3>
-  <List />
+  <h3> {people.length}birthdays today</h3>
+  <List person={people} />
   <button
   onClick={()=>console.log("you clikced me")}
   >clear all</button>
