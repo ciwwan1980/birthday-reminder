e0a113147cefd15f871e5034729c00e72438132a
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-30 00:13:58 +0200

    destructuring-from-person-&&-map-though-date

--- src/List.js
+++ src/List.js
@@ -4,10 +4,23 @@ const List = ({persons}) => {
   return (
     <>
     {persons.map((person)=>{
-  console.log(person, "person")
-    })}
+      const {id, name, age, image}=person; 
+      return (
+        <article key={id} className="person">
+          <img src={image} alt={name}/>
+          <div>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+          </div>
+
+        </article>
+      )
+    })
+
+    }
     </>
   );
 };
 
 export default List;
+
